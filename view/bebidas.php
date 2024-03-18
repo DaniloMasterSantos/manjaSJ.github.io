df30fa5b2c87150b30b38b891d1133b64064be68
DaniloMasterSantos <134393487+DaniloMasterSantos@users.noreply.github.com>
2024-03-18 17:00:05 -0300

    Adicionando todos os tipos de bebidas a página bebidas

---
+++ view/bebidas.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="css/Style_bebidas.css" rel="stylesheet">
+    <title>Pizzas</title>
+</head>
+<body>
+    <!--início do header-->
+    <header>
+        <a href="../index.html"><img src="img/logo.png" alt="Logo"></a>
+        <div class="icon-menu">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="list-header">
+            <li><a href="pizzas.php">Pizzas</a></li>
+            <li><a href="esfihas.php">Esfihas</a></li>
+            <li><a href="beirutes.php">Beirutes</a></li>
+            <li><a href="bebidas.php">Bebidas</a></li>
+        </ul>
+        <div class="icons-header">
+            <a href="#"><i class="fa-brands fa-square-whatsapp"></i></a>
+            <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+        </div>
+    </header>
+    <!--fim do header-->
+
+    <!--banners de sabores dos beirutes-->
+    <img class="banner b1" src="img/banner-sabores.png" alt="">
+    <img class="banner b2" src="img/banner-sabores-1080.jpg" alt="">
+    <img class="banner b3" src="img/banner-sabores-300.png" alt="">
+    <!--fim dos banners de sabores dos beirutes-->
+
+    <!--Todas as bebidas-->
+    <div class="box-bebidas">
+        <div class="bebidas">
+            <img src="img/b1.png" alt="#">
+            <h3>Coca Cola 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b2.png" alt="#">
+            <h3>Guaraná Antártica 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b3.png" alt="#">
+            <h3>Fanta Laranja 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b4.jpg" alt="#">
+            <h3>Fanta Uva 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b5.webp" alt="#">
+            <h3>Sprite ou Soda 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b6.jpg" alt="#">
+            <h3>Dolly Guaraná e Limão 2L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b7.jfif" alt="#">
+            <h3>Sukita Tubaína 2L</h3>
+        </div>
+        
+        <div class="bebidas">
+            <img src="img/b9.webp" alt="#">
+            <h3>Suco de 1L</h3>
+        </div>
+
+        <div class="bebidas">
+            <img src="img/b8.png" alt="#">
+            <h3>Refrigerante e cerveja em Lata</h3>
+        </div>
+    </div>
+    
+    <!--Fim Todas as bebidas-->
+
+    <!--Footer-->
+    <footer>
+        <a href="../index.html"><img src="img/logo.png" alt="Logo"></a>
+        <div class="icons-footer">
+            <a href="#"><i class="fa-brands fa-square-whatsapp"></i></a>
+            <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+        </div>
+    </footer>
+    <!--Fim do Footer-->
+
+
+    <script src="js/responsividade_menu.js"></script>
+</body>
+</html>
